8a5aac6a7c4f71fa07c00adbed5b30ced1bc65c6
10164507110 <1071610321@qq.com>
2018-12-13 00:00:36 +0800

    第四次作业提交，增加服务器，实现刷新点名功能“

---
+++ my_server/lib/channel.dart
@@ -0,0 +1,54 @@
+import 'my_server.dart';
+import 'dart:math';
+/// This type initializes an application.
+///
+/// Override methods in this class to set up routes and initialize services like
+/// database connections. See http://aqueduct.io/docs/http/channel/.
+class MyServerChannel extends ApplicationChannel {
+  /// Initialize services in this method.
+  ///
+  /// Implement this method to initialize services, read values from [options]
+  /// and any other initialization required before constructing [entryPoint].
+  ///
+  /// This method is invoked prior to [entryPoint] being accessed.
+  @override
+  Future prepare() async {
+    logger.onRecord.listen((rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
+  }
+
+  /// Construct the request channel.
+  ///
+  /// Return an instance of some [Controller] that will be the initial receiver
+  /// of all [Request]s.
+  ///
+  /// This method is invoked after [prepare].
+  @override
+  Controller get entryPoint {
+    final router = Router();
+
+    // Prefer to use `link` instead of `linkFunction`.
+    // See: https://aqueduct.io/docs/http/request_controller/
+    router
+      .route("/users/[:id]").link(()=>MyController());
+      ;
+
+    return router;
+  }
+}
+class MyController extends ResourceController {
+  final List<String> things = ['张静雅','唐莉雯','龙晶毅','吴松二','蔡心蕊','李典康','周嘉翔','戚晓颖','郑可欣','陈瑶','赵世宇','朱子恒'];
+var random = Random();
+  var number=random.nextInt(12);
+  @Operation.get()
+  Future<Response> getThings() async {
+    return Response.ok(things[number]);
+  }
+
+  @Operation.get('id')
+  Future<Response> getThing(@Bind.path('id') int id) async {
+    if (id < 0 || id >= things.length) {
+      return Response.notFound();
+    }
+    return Response.ok(things[id]);
+  }
+}
